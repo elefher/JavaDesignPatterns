813b8cb2ef5a70901d4d6209480093acc8e66f98
elefher <eldelielev@gmail.com>
2017-04-12 21:36:47 +0300

    create Head interface. 2 implementations. regular head and big head

---
+++ src/main/java/codinginsights/practice/DependencyInjection/BigHead.java
@@ -0,0 +1,13 @@
+package codinginsights.practice.DependencyInjection;
+
+/**
+ * Created by elefher on 4/12/17.
+ */
+
+public class BigHead implements Head {
+
+  @Override
+  public String headType() {
+	return "big head";
+  }
+}
---
+++ src/main/java/codinginsights/practice/DependencyInjection/RegularHead.java
@@ -0,0 +1,13 @@
+package codinginsights.practice.DependencyInjection;
+
+/**
+ * Created by elefher on 4/12/17.
+ */
+
+public class RegularHead implements Head {
+
+  @Override
+  public String headType() {
+	return "regular head";
+  }
+}
